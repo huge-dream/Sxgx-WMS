6becfeb5d04b303d926a754d3da46072b7413682
china_ahhui <ahhuiguys@163@.com>
2022-11-04 21:17:15 +0800

    修改初始化的地址数据

--- userregister/views.py
+++ userregister/views.py
@@ -18,8 +18,10 @@ def randomPhone():
                "176", "186", "187", "188", "189"]
     return (random.choice(List) + "".join(random.choice("0123456789") for i in range(8)))
 
-randomcity = ["shanghai", "nanjing", "hangzhou", "beijing", "chongqing", "shenzhen", "guangzhou", "suzhou", "hefei",
-                "chengdu", "kunming", "wuhan"]
+# randomcity = ["shanghai", "nanjing", "hangzhou", "beijing", "chongqing", "shenzhen", "guangzhou", "suzhou", "hefei",
+#                 "chengdu", "kunming", "wuhan"]
+randomcity = ["AA-BB", "CC-DD", "EE-FF", "GG-HH", "II-JJ", "KK-LL", "MM-NN", "OO-PP", "QQ-RR", "SS-TT", "UU-VV", "WW-XX",
+              "YY-ZZ"]
 
 randomcolor = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple"]
 
