a9c69f1dda56c78c55f2d271a50502eb87f91123
李强 <1206709430@qq.com>
2022-11-16 10:03:06 +0800

    更新

--- in_out_warehouse/views.py
+++ in_out_warehouse/views.py
@@ -1,5 +1,3 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import serializers, status
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
@@ -15,15 +13,15 @@ class InOutWarehouseSerializer(ModelSerializer):
     """
     出入库表-序列化器
     """
-    goods_code = serializers.CharField(read_only=True,source='good.goods_code')
+    goods_code = serializers.CharField(read_only=True, source='good.goods_code')
     goods_desc = serializers.CharField(read_only=True, source='good.goods_desc')
     binset_name = serializers.CharField(read_only=True, source='binset.bin_name')
     type_label = serializers.CharField(read_only=True, source='get_type_display')
+
     class Meta:
         model = ListModel
         fields = "__all__"
         read_only_fields = ["id"]
-
 
 
 class InOutWarehouseViewSet(ModelViewSet):
@@ -35,8 +33,8 @@ class InOutWarehouseViewSet(ModelViewSet):
     pagination_class = MyPageNumberPagination
     filter_backends = [DjangoFilterBackend, OrderingFilter, ]
     ordering_fields = ['id', "create_time", "update_time", ]
-    filter_fields =['type']
-    search_fields = ['good__goods_code','good__goods_desc']
+    filter_fields = ['type']
+    search_fields = ['good__goods_code', 'good__goods_desc']
 
     def get_serializer(self, *args, **kwargs):
         serializer_class = self.get_serializer_class()
@@ -55,22 +53,10 @@ class InOutWarehouseViewSet(ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         # 写这里
+        if serializer.instance.type == 0:
+            # 入库
+            pass
+        else:
+            # 出库
+            pass
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def update(self, request, *args, **kwargs):
-        """
-        编辑
-        """
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        if getattr(instance, '_prefetched_objects_cache', None):
-            instance._prefetched_objects_cache = {}
-        # 写这里
-        return Response(serializer.data)
-
-
-
-
